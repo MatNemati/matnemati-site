e0ff0471419dbad8eadf93151605d4fb96f932af
Matin Nemati <95319513+MatNemati@users.noreply.github.com>
2025-08-29 18:47:26 +0330

    Update SmoothScroll.tsx

--- components/SmoothScroll.tsx
+++ components/SmoothScroll.tsx
@@ -6,7 +6,7 @@ import Lenis from 'lenis'
 
 export default function SmoothScroll() {
   useEffect(() => {
-    const lenis = new Lenis({ smoothWheel: true, smoothTouch: false, duration: 1.1 })
+    const lenis = new Lenis({ smoothWheel: true, duration: 1.1 })
     function raf(time: number) {
       lenis.raf(time)
       requestAnimationFrame(raf)
